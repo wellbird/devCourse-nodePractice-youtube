cb297baeac9038a3018d263f3406d7cd9528682d
Jaewon <wellbird@naver.com>
2024-12-02 23:00:55 +0900

    feat : 채널 파트 db 연동

--- routes/channel.js
+++ routes/channel.js
@@ -1,12 +1,11 @@
 const express = require('express');
+const conn = require('../mariaDB');
 
 const router = express.Router();
 
 router.use(express.static('public'));
 router.use(express.json());
 router.use(express.urlencoded({ extended: true }));
-
-const db = new Map();
 
 router.route('/').get((req, res) => {
   const userId = req.session.userId;
@@ -19,53 +18,58 @@ router.route('/').get((req, res) => {
       `);
   }
 
-  const channels = [...db.values()].filter((channel) => channel.userId === userId);
-
-  const channelList = channels
-    .map((channel) => {
-      return `
-      <div class="channel-item">
-        <p>${channel.title}</p>
-        <p>동영상 ${channel.videoCount}개</p>
-        <p>구독자 ${channel.subscribeCount}명</p>
-        <div class="button-container">
-          <button onclick="location.href='/channel/edit/${channel.title}'">수정</button>
-          <button onclick="confirmDelete('${channel.title}')">삭제</button>
+  const sql = 'SELECT * FROM channels WHERE user_id = ?';
+  conn.query(sql, userId, (err, results, fields) => {
+    if (err) {
+      return res.status(404).json({ message: '알 수 없는 에러가 발생하였습니다.' });
+    } else {
+      const channelList = results
+        .map((channel) => {
+          return `
+        <div class="channel-item">
+          <p>${channel.title}</p>
+          <p>동영상 ${channel.video_count}개</p>
+          <p>구독자 ${channel.subscribe_count}명</p>
+          <div class="button-container">
+            <button onclick="location.href='/channel/edit/${channel.title}'">수정</button>
+            <button onclick="confirmDelete('${channel.title}')">삭제</button>
+          </div>
         </div>
-      </div>
-    `;
-    })
-    .join('');
+      `;
+        })
+        .join('');
 
-  res.send(`
-    <!DOCTYPE html>
-    <html lang="kr">
-        <head>
-        <meta charset="UTF-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <link rel="stylesheet" href="/css/style.css" />
-        <script src="/js/deleteChannel.js"></script>
-        <title>채널 관리</title>
-        </head>
-        <body>
-        <div class="container">
-            <h1>채널 관리</h1>
-            <button onclick="location.href='/channel/create'">채널 생성</button>
-            <hr/>
-            <h3>내 채널 목록</h3>
-            ${channels.length > 0 ? channelList : '<p>채널이 없습니다.</p>'}
-        </div>
-        <script>
-            function confirmDelete(title) {
-            const confirmation = confirm('정말로 삭제하시겠습니까?');
-            if (confirmation) {
-                deleteChannel(title);
-            }
-            }
-        </script>
-        </body>
-    </html>
-  `);
+      res.send(`
+        <!DOCTYPE html>
+        <html lang="kr">
+            <head>
+            <meta charset="UTF-8" />
+            <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+            <link rel="stylesheet" href="/css/style.css" />
+            <script src="/js/deleteChannel.js"></script>
+            <title>채널 관리</title>
+            </head>
+            <body>
+            <div class="container">
+                <h1>채널 관리</h1>
+                <button onclick="location.href='/channel/create'">채널 생성</button>
+                <hr/>
+                <h3>내 채널 목록</h3>
+                ${results.length > 0 ? channelList : '<p>채널이 없습니다.</p>'}
+            </div>
+            <script>
+                function confirmDelete(title) {
+                const confirmation = confirm('정말로 삭제하시겠습니까?');
+                if (confirmation) {
+                    deleteChannel(title);
+                }
+                }
+            </script>
+            </body>
+        </html>
+      `);
+    }
+  });
 });
 
 router
@@ -117,14 +121,14 @@ router
 
     const { title } = req.body;
 
-    if (db.has(title)) {
-      return res.status(400).json({ message: '중복된 채널명입니다.' });
-    }
-
-    const newChannel = { title, userId: userId, videoCount: 0, subscribeCount: 0 };
-    db.set(title, newChannel);
-
-    res.status(200).json({ message: '채널이 생성되었습니다.' });
+    const sql = 'INSERT INTO channels (title, user_id) VALUES (?, ?)';
+    conn.query(sql, [title, userId], (err, results, fields) => {
+      if (err) {
+        return res.status(401).json({ message: '중복된 채널명입니다.' });
+      } else {
+        return res.status(200).json({ message: '채널이 생성되었습니다.' });
+      }
+    });
   });
 
 router
@@ -142,36 +146,37 @@ router
     }
 
     const title = req.params.title;
-    const channel = db.get(title);
 
-    if (channel.userId !== userId) {
-      return res.status(404).json({ message: '채널 수정 권한이 없습니다.' });
-    }
-    if (!channel) {
-      return res.status(404).json({ message: '채널을 찾을 수 없습니다.' });
-    }
-
-    res.send(`
-      <!DOCTYPE html>
-      <html lang="kr">
-        <head>
-          <meta charset="UTF-8" />
-          <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-          <link rel="stylesheet" href="/css/style.css" />
-          <script src="/js/putChannel.js"></script>
-          <title>채널 수정</title>
-        </head>
-        <body>
-          <div class="container">
-            <h1>채널 수정</h1>
-            <form onsubmit="return putChannel(event, '${channel.title}')">
-              <input type="text" name="title" id="title" value="${channel.title}" required />
-              <button type="submit">수정</button>
-            </form>
-          </div>
-        </body>
-      </html>
-    `);
+    const sql = 'SELECT * FROM channels WHERE title = ?';
+    conn.query(sql, title, (err, results, fields) => {
+      if (err) {
+        return res.status(404).json({ message: '채널 정보를 찾을 수 없습니다.' });
+      } else {
+        const channelInfo = results[0];
+        if (userId !== channelInfo.user_id) return res.status(404).json({ message: '채널 수정 권한이 없습니다.' });
+        return res.send(`
+          <!DOCTYPE html>
+          <html lang="kr">
+            <head>
+              <meta charset="UTF-8" />
+              <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+              <link rel="stylesheet" href="/css/style.css" />
+              <script src="/js/putChannel.js"></script>
+              <title>채널 수정</title>
+            </head>
+            <body>
+              <div class="container">
+                <h1>채널 수정</h1>
+                <form onsubmit="return putChannel(event, '${channelInfo.title}')">
+                  <input type="text" name="title" id="title" value="${channelInfo.title}" required />
+                  <button type="submit">수정</button>
+                </form>
+              </div>
+            </body>
+          </html>
+        `);
+      }
+    });
   })
   .put((req, res) => {
     const userId = req.session.userId;
@@ -185,31 +190,26 @@ router
     }
 
     const title = req.params.title;
-    const channel = db.get(title);
 
-    if (channel.userId !== userId) {
-      return res.status(404).json({ message: '채널 수정 권한이 없습니다.' });
-    }
-    if (!channel) {
-      return res.status(404).json({ message: '채널을 찾을 수 없습니다.' });
-    }
+    const getSql = 'SELECT * FROM channels WHERE title = ?';
+    const updateSql = 'UPDATE channels SET title = ? WHERE title = ?';
 
-    const { newTitle } = req.body;
-
-    if (db.has(newTitle)) {
-      return res.status(400).json({ message: '중복된 채널명입니다.' });
-    }
-
-    db.delete(title);
-    const updatedChannel = {
-      title: newTitle,
-      userId: userId,
-      videoCount: channel.videoCount,
-      subscribeCount: channel.subscribeCount,
-    };
-    db.set(newTitle, updatedChannel);
-
-    res.status(200).json({ message: '채널이 수정되었습니다.' });
+    conn.query(getSql, title, (err, results, fields) => {
+      if (err) {
+        return res.status(404).json({ message: '채널 정보를 찾을 수 없습니다.' });
+      } else {
+        const channelInfo = results[0];
+        if (userId !== channelInfo.user_id) return res.status(404).json({ message: '채널 수정 권한이 없습니다.' });
+        const { newTitle } = req.body;
+        return conn.query(updateSql, [newTitle, title], (err, results, fields) => {
+          if (err) {
+            return res.status(401).json({ message: '중복된 채널명입니다.' });
+          } else {
+            return res.status(200).json({ message: '채널이 수정되었습니다.' });
+          }
+        });
+      }
+    });
   });
 
 router.route('/delete/:title').delete((req, res) => {
@@ -224,17 +224,29 @@ router.route('/delete/:title').delete((req, res) => {
   }
 
   const title = req.params.title;
-  const channel = db.get(title);
 
-  if (channel.userId !== userId) {
-    return res.status(404).json({ message: '채널 삭제 권한이 없습니다.' });
-  }
-  if (!channel) {
-    return res.status(404).json({ message: '채널을 찾을 수 없습니다.' });
-  }
+  const getSql = 'SELECT * FROM channels WHERE title = ?';
+  const deleteSql = 'DELETE FROM channels WHERE title = ?';
 
-  db.delete(title);
-  res.status(200).json({ message: '채널이 삭제되었습니다.' });
+  conn.query(getSql, title, (err, results, fields) => {
+    if (err) {
+      return res.status(404).json({ message: '채널을 찾을 수 없습니다.' });
+    } else {
+      const channelInfo = results[0];
+      if (userId !== channelInfo.user_id) return res.status(404).json({ message: '채널 삭제 권한이 없습니다.' });
+      return conn.query(deleteSql, title, (err, results, fields) => {
+        if (err) {
+          return res.status(404).json({ message: '채널을 찾을 수 없습니다.' });
+        } else {
+          if (results.affectedRows === 1) {
+            return res.status(200).json({ message: '채널 삭제가 완료되었습니다.' });
+          } else {
+            return res.status(404).json({ message: '존재하지 않는 채널입니다.' });
+          }
+        }
+      });
+    }
+  });
 });
 
 module.exports = router;
